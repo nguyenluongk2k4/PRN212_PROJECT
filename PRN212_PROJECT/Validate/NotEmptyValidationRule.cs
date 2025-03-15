8c13d3382d052740defae06aa6686bc58b46fbb4
nguyenluongk2k4 <nguyenluongk2k4@gmail.com>
2025-03-15 12:02:11 +0700

    Add project files.

---
+++ PRN212_PROJECT/Validate/NotEmptyValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PRN212_PROJECT.Validate
+{
+    public class NotEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string input = value as string;
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return new ValidationResult(false, "This field cannot be empty.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
